0a5a0213de8a55a5c01e7ac3dfd55e1cdb7b0c91
Chikennannbann <kamsk517@gmail.com>
2022-04-10 14:38:00 +0000

    add destroy

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   post 'create' => 'books#create'
   get ':id' => 'books#show', as: 'book'
-  get ':id/edit' => 'books#edit', as: 'edit_list'
+  get ':id/edit' => 'books#edit', as: 'edit_book'
+  patch ':id' => 'books#update', as: 'update_book'
+  delete ':id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
