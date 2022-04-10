2b0b48389b03ce4ffe5fc6e9067a57caa4f6ad0a
Chikennannbann <kamsk517@gmail.com>
2022-04-10 04:51:35 +0000

    add create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+    root to: 'homes#top'
+    # get 'homes/top'ではなく.com/でトップ画面を表示するルーティング
+  get 'index' => 'books#index'
+  post 'books' => 'books#create'
+  get 'show' => 'books#show'
+  get 'edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
